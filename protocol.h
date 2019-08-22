5cb0273017e38007d81b26eae690a1333edb9472
zfl9 <zfl9.com@gmail.com>
2019-08-22 19:22:20 +0800

    update protocol.h

--- protocol.h
+++ protocol.h
@@ -5,13 +5,13 @@
 #include "netutils.h"
 #undef _GNU_SOURCE
 
-#define MSGTYPE_HEARTBEAT 0x00
-#define MSGTYPE_CONNECT4  0x01
-#define MSGTYPE_CONNECT6  0x02
-#define MSGTYPE_ESTABLISH 0x03
-#define MSGTYPE_PAYLOAD   0x04
-#define MSGTYPE_CLOSE     0x05
-#define MSGTYPE_ERROR     0x06
+#define MSGTYPE_HEARTBEAT 0x00 /* tun-server heartbeat (on demand) */
+#define MSGTYPE_CONNECT4  0x01 /* ipv4 connection request (async) */
+#define MSGTYPE_CONNECT6  0x02 /* ipv6 connection request (async) */
+#define MSGTYPE_ESTABLISH 0x03 /* connection established notice */
+#define MSGTYPE_PAYLOAD   0x04 /* application layer payload */
+#define MSGTYPE_CLOSE     0x05 /* connection close notice */
+#define MSGTYPE_ERROR     0x06 /* an error occurred */
 
 typedef struct {
     uint8_t type;
@@ -19,40 +19,40 @@ typedef struct {
 
 typedef struct {
     uint8_t   type;
-    uint64_t  sptr;
-    ipaddr4_t addr;
-    portno_t  port;
+    uint64_t  sptr; /* source object pointer */
+    ipaddr4_t addr; /* target ipv4 address */
+    portno_t  port; /* target port number */
 } __attribute__((packed)) msg_connect4_t;
 
 typedef struct {
     uint8_t   type;
-    uint64_t  sptr;
-    ipaddr6_t addr;
-    portno_t  port;
+    uint64_t  sptr; /* source object pointer */
+    ipaddr6_t addr; /* target ipv6 address */
+    portno_t  port; /* target port number */
 } __attribute__((packed)) msg_connect6_t;
 
 typedef struct {
     uint8_t  type;
-    uint64_t sptr;
-    uint64_t dptr;
+    uint64_t sptr; /* source object pointer */
+    uint64_t dptr; /* target object pointer */
 } __attribute__((packed)) msg_establish_t;
 
 /* with payload */
 typedef struct {
     uint8_t  type;
-    uint64_t ptr;
-    uint32_t len;
-    uint8_t  data[]; // sizeof = 0
+    uint64_t ptr; /* associated object pointer */
+    uint32_t len; /* the length of the payload */
+    uint8_t  data[]; /* payload (sizeof is zero) */
 } __attribute__((packed)) msg_payload_t;
 
 typedef struct {
     uint8_t  type;
-    uint64_t ptr;
+    uint64_t ptr; /* associated object pointer */
 } __attribute__((packed)) msg_close_t;
 
 typedef struct {
     uint8_t  type;
-    uint64_t ptr;
+    uint64_t ptr; /* associated object pointer */
 } __attribute__((packed)) msg_error_t;
 
 #endif
