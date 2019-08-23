45dfc9e44b6f64c37ce91ba16c72f3dcc3f1c0b4
zfl9 <zfl9.com@gmail.com>
2019-08-23 11:59:46 +0800

    update tunclient/tunserver

---
+++ tunclient.c
@@ -0,0 +1,12 @@
+#define _GNU_SOURCE
+#include "constant.h"
+#include "hashset.h"
+#include "logutils.h"
+#include "netutils.h"
+#include "protocol.h"
+#undef _GNU_SOURCE
+
+int main() {
+    LOGINF("[main] hello, world");
+    return 0;
+}
