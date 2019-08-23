6205cde87827c0a2c78c62f61010de01d387bd40
zfl9 <zfl9.com@gmail.com>
2019-08-23 12:42:08 +0800

    update tunclient/tunserver

--- tunclient.c
+++ tunclient.c
@@ -4,6 +4,17 @@
 #include "logutils.h"
 #include "netutils.h"
 #include "protocol.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
 #undef _GNU_SOURCE
 
 int main() {
