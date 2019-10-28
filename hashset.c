7d9b9f7f157187aa591ebc0ff52185ae5aa93c04
zfl9 <zfl9.com@gmail.com>
2019-10-28 18:44:53 +0800

    update hashset.c hashset.h

--- hashset.c
+++ hashset.c
@@ -3,22 +3,26 @@
 #include <stdlib.h>
 #undef _GNU_SOURCE
 
+/* create an empty hashset */
 hashset_t* hashset_new(void) {
     return calloc(1, sizeof(hashset_t));
 }
 
+/* add an element into hashset */
 void hashset_add(hashset_t *set, void *elem) {
     hashentry_t *entry = malloc(sizeof(hashentry_t));
     entry->elem = elem;
     HASH_ADD_PTR(set->head, elem, entry);
 }
 
+/* check if a given element exists */
 bool hashset_exist(hashset_t *set, void *elem) {
     hashentry_t *entry = NULL;
     HASH_FIND_PTR(set->head, &elem, entry);
     return entry != NULL;
 }
 
+/* remove an element from hashset */
 void hashset_remove(hashset_t *set, void *elem) {
     hashentry_t *entry = NULL;
     HASH_FIND_PTR(set->head, &elem, entry);
@@ -27,6 +31,7 @@ void hashset_remove(hashset_t *set, void *elem) {
     free(entry);
 }
 
+/* remove all elements from hashset */
 void hashset_clear(hashset_t *set, hashelem_free_cb freecb) {
     hashentry_t *current_entry = NULL, *temp_entry = NULL;
     HASH_ITER(hh, set->head, current_entry, temp_entry) {
@@ -36,6 +41,7 @@ void hashset_clear(hashset_t *set, hashelem_free_cb freecb) {
     }
 }
 
+/* remove all elements from hashset and free `set` */
 void hashset_free(hashset_t *set, hashelem_free_cb freecb) {
     hashset_clear(set, freecb);
     free(set);
